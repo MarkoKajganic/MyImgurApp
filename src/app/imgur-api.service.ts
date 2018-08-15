12b255cc65ec35da3c6d2a5635d87b03af81fa0e
MarkoKajganic <kajganicmarko@gmail.com>
2018-08-15 13:06:02 +0200

    moved variables to env

--- src/app/imgur-api.service.ts
+++ src/app/imgur-api.service.ts
@@ -8,28 +8,30 @@ import { environment } from '../environments/environment';
 @Injectable()
 export class ImgurApiService {
   baseUrl = environment.baseUrl;
+  clientId = environment.clientId;
+  accessToken = environment.acessToken;
 
   constructor(private http: HttpClient) { }
 
  
   getAlbumsIds(): Observable<any> {
-    let headers = new HttpHeaders().set('Authorization', 'Client-ID a8f49cff5adf72a');
+    let headers = new HttpHeaders().set('Authorization', this.clientId);
     return this.http.get(`${this.baseUrl}/3/account/MarkoKajganic/albums/ids`, {headers})
                     .map(response => response);
   }
 
   getAlbum(id): Observable<any> {
-    let headers = new HttpHeaders().set('Authorization', 'Bearer 3620072df3d803fa82a4cc8796550d9b79bd2123');
+    let headers = new HttpHeaders().set('Authorization', this.accessToken);
     return this.http.get(`${this.baseUrl}/3/account/MarkoKajganic/album/${id}`, {headers});
   }
 
   favoriteImage(id): Observable<any> {
-    let headers = new HttpHeaders().set('Authorization', 'Bearer 3620072df3d803fa82a4cc8796550d9b79bd2123');
+    let headers = new HttpHeaders().set('Authorization', this.accessToken);
     return this.http.post(`${this.baseUrl}/3/image/${id}/favorite`, {headers});
   }
 
   getFavoriteImages(): Observable<any> {
-    let headers = new HttpHeaders().set('Authorization', 'Bearer 3620072df3d803fa82a4cc8796550d9b79bd2123');
+    let headers = new HttpHeaders().set('Authorization', this.accessToken);
     return this.http.get(`${this.baseUrl}/3/account/MarkoKajganic/favorites`, {headers})
   }
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,5 +6,7 @@
 export const environment = {
   production: false,
   baseUrl: 'https://api.imgur.com',
+  clientId: 'Client-ID a8f49cff5adf72a',
+  acessToken: 'Bearer 3620072df3d803fa82a4cc8796550d9b79bd2123',
   
 };
